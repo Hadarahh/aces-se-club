a233fade001db33a23b7f4bc6067eed80f1370ef
ohwojeheri glorious <ohwojeheriglorious608@gmail.com>
2025-02-26 07:30:14 +0100

    Print first 5 armstrong or happy number

--- Back-End/Operators, control flow/task.js
+++ Back-End/Operators, control flow/task.js
@@ -31,18 +31,26 @@ function happyNumber(num) {
     let sum = addSquareOfDigits(num);
 
     if (sum === 1) {
-        console.log("happy");
-        return;
+        return "happy";
     };
     if (sum !== 1 && String(sum).length == 1) {
-        console.log("sad");
-        return;
+        return "sad";
     };
 
     return happyNumber(sum);
 }
 
-happyNumber(19);
+let happyArr = [], index1 = 0;;
+while (happyArr.length < 5) {
+    let str = happyNumber(index1);
+
+    if (str === "happy") {
+        happyArr.push(index1);
+    }
+    index1++;
+};
+
+console.log(happyArr);
 
 
 
@@ -61,13 +69,21 @@ function armstrong(num) {
     let sumOfCubes = addCubeOfDigits(num);
 
     if (sumOfCubes === num) {
-        console.log("Armstrong number");
-    } else {
-        console.log("Not an Armstrong number")
+        return "arm";
     };
-}
+};
 
-armstrong(17);
+let armArr = [], index2 = 0;
+while (armArr.length < 5) {
+    let str = armstrong(index2);
+
+    if (str === "arm") {
+        armArr.push(index2);
+    }
+    index2++;
+};
+
+console.log(armArr);
 
 
 // Array sort
@@ -76,63 +92,6 @@ function arraySort(arr) {
     if (!Array.isArray(arr)) {
         console.log("Not an array");
     } else {
-        function toBigCase(array) {
-            arr.forEach((element, index) => {
-                if ((typeof element) === "string") {
-                    arr.splice(index, 1, element.toUpperCase());
-                }
-            });
-        };
-        toBigCase(arr);
-
-        arr.sort((a, b) => {
-            let typeA = typeof a, typeB = typeof b;
-
-            if ((typeA === typeB)) {
-                if (typeA === "string") {
-                    return a.localeCompare(b);
-                } else if ((typeA === "number")) {
-                    return a - b;
-                } else {
-                    return 0;
-                }
-            } else {
-                return typeA.localeCompare(typeB);
-            };
-        });
-
-        console.log(arr);
-    };
+        arr.sort((a, b) => a - b);
+    }
 };
-
-let arr = [,
-    "string",
-    "Last",
-    5, 7, 0, 30, 25,
-    "apple",
-    {
-        street: "2nd street",
-        number: "123",
-        zipcode: "33116",
-        city: "Miami",
-        state: "Florida",
-        onemore: "yess"
-    },
-    {
-        street: "1st West avenue",
-        number: "5",
-        zipcode: "75001",
-        city: "Addison",
-        state: "Texas"
-    },
-    true,
-    undefined,
-    false,
-    null,
-    function checkString(element, index) {
-        return typeof element === "string";
-    },
-    [2, 3, 'youuu']
-];
-
-arraySort(arr);
